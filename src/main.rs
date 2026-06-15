d4237d60c4ad39a98fedb27d6fdc1af281c006a4
Pogwat <208688662+Pogwat@users.noreply.github.com>
2026-06-14 22:56:35 -0400

    make methods public

--- src/main.rs
+++ src/main.rs
@@ -16,28 +16,28 @@ fn main() {
 
 use bit_field::BitField;
 
-trait UInts {const ELEMENT_BITS:usize;}
+pub trait UInts {const ELEMENT_BITS:usize;}
 macro_rules! unints {($($type:ty),*) => {
     $(impl UInts for $type {const ELEMENT_BITS:usize=std::mem::size_of::<$type>() * 8;})*}
 }
 unints!(u8,u16,u32,u64);
 
 struct Bitys<ElementType: UInts> {
-    bytes:Vec<ElementType>,
-    set_bits:usize
+    pub bytes:Vec<ElementType>,
+    pub set_bits:usize
 }
 
 impl<ElementType: UInts + BitField + Default + Clone + Copy> Bitys<ElementType> {
-    fn bit_idx(bitdex:usize) -> usize {bitdex%ElementType::ELEMENT_BITS}
-    fn elems_idx(bitdex:usize) -> usize {bitdex/ElementType::ELEMENT_BITS}
-    fn missalignment(&self) -> usize {Self::bit_idx(self.set_bits)}
-    fn get(&self, index:usize) -> bool {self.bytes[Self::elems_idx(index)].get_bit(Self::bit_idx(index))}
-    fn new_empty_elem(&mut self) {self.bytes.push(ElementType::default())}
-    fn new_empty_elems(&mut self, empty_elems:usize) {self.bytes.resize(self.bytes.len()+empty_elems,ElementType::default())}
-    fn set(&mut self, bitdex:usize, val:bool) {
+    pub fn bit_idx(bitdex:usize) -> usize {bitdex%ElementType::ELEMENT_BITS}
+    pub fn elems_idx(bitdex:usize) -> usize {bitdex/ElementType::ELEMENT_BITS}
+    pub fn missalignment(&self) -> usize {Self::bit_idx(self.set_bits)}
+    pub fn get(&self, index:usize) -> bool {self.bytes[Self::elems_idx(index)].get_bit(Self::bit_idx(index))}
+    pub fn new_empty_elem(&mut self) {self.bytes.push(ElementType::default())}
+    pub fn new_empty_elems(&mut self, empty_elems:usize) {self.bytes.resize(self.bytes.len()+empty_elems,ElementType::default())}
+    pub fn set(&mut self, bitdex:usize, val:bool) {
         let elems_idx = Self::elems_idx(bitdex);
         if elems_idx+1>self.bytes.len() {self.new_empty_elems(elems_idx+1-self.bytes.len())} //index+1>self.len() == index>self.len()-1, algebra add 1 to both sides, thsi repvents underflow if self.len()==0, as 0-1 = -1 usize cant be negative
         self.bytes[elems_idx].set_bit(Self::bit_idx(bitdex),val);
     }
-    fn new() -> Self{ Self{bytes:Vec::new(), set_bits:0}  }
+    pub fn new() -> Self{ Self{bytes:Vec::new(), set_bits:0}  }
 } //Excluding genercis I have a full working bitvec in lines:17-35 just 18 lines of code!
