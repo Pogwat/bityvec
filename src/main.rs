991f3c07878fe51bcb43696414421b4436500299
Pogwat <208688662+Pogwat@users.noreply.github.com>
2026-06-14 23:02:52 -0400

    remove set_bits field

--- src/main.rs
+++ src/main.rs
@@ -24,13 +24,11 @@ unints!(u8,u16,u32,u64);
 
 struct Bitys<ElementType: UInts> {
     pub bytes:Vec<ElementType>,
-    pub set_bits:usize
 }
 
 impl<ElementType: UInts + BitField + Default + Clone + Copy> Bitys<ElementType> {
     pub fn bit_idx(bitdex:usize) -> usize {bitdex%ElementType::ELEMENT_BITS}
     pub fn elems_idx(bitdex:usize) -> usize {bitdex/ElementType::ELEMENT_BITS}
-    pub fn missalignment(&self) -> usize {Self::bit_idx(self.set_bits)}
     pub fn get(&self, index:usize) -> bool {self.bytes[Self::elems_idx(index)].get_bit(Self::bit_idx(index))}
     pub fn new_empty_elem(&mut self) {self.bytes.push(ElementType::default())}
     pub fn new_empty_elems(&mut self, empty_elems:usize) {self.bytes.resize(self.bytes.len()+empty_elems,ElementType::default())}
@@ -39,5 +37,5 @@ impl<ElementType: UInts + BitField + Default + Clone + Copy> Bitys<ElementType> 
         if elems_idx+1>self.bytes.len() {self.new_empty_elems(elems_idx+1-self.bytes.len())} //index+1>self.len() == index>self.len()-1, algebra add 1 to both sides, thsi repvents underflow if self.len()==0, as 0-1 = -1 usize cant be negative
         self.bytes[elems_idx].set_bit(Self::bit_idx(bitdex),val);
     }
-    pub fn new() -> Self{ Self{bytes:Vec::new(), set_bits:0}  }
+    pub fn new() -> Self{ Self{bytes:Vec::new()}  }
 } //Excluding genercis I have a full working bitvec in lines:17-35 just 18 lines of code!
