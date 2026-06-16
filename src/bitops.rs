bfd6ede6470e96b06ea186c2e1b3037dcef45eaa
Pogwat <208688662+Pogwat@users.noreply.github.com>
2026-06-15 22:17:26 -0400

    ranger

--- src/bitops.rs
+++ src/bitops.rs
@@ -14,19 +14,9 @@ macro_rules! bittypes {
             impl BitTypes for $type {}
 
             impl BitOps for $type {
-                fn bitmask<R:RangeBounds<usize>>(self, range:R) -> Self { //indexes: 0..=Self::BITS-1
-                let start = match range.start_bound() {
-                    Bound::Included(&val) => val,
-                    Bound::Excluded(&val) => val + 1, // Rare in standard Rust ranges, but possible
-                    Bound::Unbounded => 1           // e.g., ..5 starts at index 0
-                }.min(0);
-
-                // 2. Determine the last element index
-                let end = match range.end_bound() {
-                    Bound::Included(&val) => val,
-                    Bound::Excluded(&val) => val.saturating_sub(1),
-                    Bound::Unbounded =>  (Self::BITS as usize)-1
-                }.max((Self::BITS as usize)-1);
+                fn bitmask<R:RangeBounds<usize>+ NumRangeExtract<usize>>(self, range:R) -> Self { //indexes: 0..=Self::BITS-1
+                let start = range.start().unwrap_or(0).min(0);
+                let end = range.end().unwrap_or(Self::BITS as usize).max((Self::BITS as usize)-1);
                 let upper_mask:Self =(1<<end)-1; //All bits upto including last
                 let lower_mask:Self =(1<<start)-1; //All bits before start
                 upper_mask^lower_mask //Clear mask overlapping bits
@@ -35,11 +25,48 @@ macro_rules! bittypes {
                 fn get_bit(self, bitdex:usize) -> bool {(self & 1<<bitdex) !=0 }
                 fn set_bit(&mut self, bitdex:usize) {*self |= 1<<bitdex}
                 //fn unset_bit(&mut self, bitdex:usize) {*self |= !(1<<bitdex)}
+
+
+
             }
         )*
     }
 }
 bittypes!(u8,u16,u32,u64,usize);
+
+pub trait NumRangeExtract<T>: RangeBounds<T>  {
+    fn end(&self) -> Option<T>;
+    fn start(&self) -> Option<T>;
+}
+
+
+
+macro_rules! num_rangy {
+    ($($type:ty),*) => {
+        $(
+            impl <R:RangeBounds<$type>>NumRangeExtract<$type> for R {
+                fn end(&self) -> Option<$type> {
+                    match self.end_bound() {
+                        Bound::Included(val) => Some(*val),
+                        Bound::Excluded(val) => Some((*val).saturating_sub(1)),
+                        Bound::Unbounded =>  None
+                    }
+                }
+                fn start(&self) -> Option<$type> {
+                    match self.start_bound() {
+                        Bound::Included(val) => Some(*val),
+                        Bound::Excluded(val) => Some((*val).saturating_add(1)), // Rare in standard Rust ranges, but possible
+                        Bound::Unbounded => None           // e.g., ..5 starts at index 0
+                    }
+                }
+            }
+        )*
+    }
+}
+
+num_rangy!(u8,u16,u32,u64,usize);
+
+
 
 use std::ops::Bound;
 use std::ops::RangeBounds;
--- tests/test.rs
+++ tests/test.rs
@@ -12,3 +12,21 @@ fn get_set() {
     assert_eq!(bitys_test.get(4),false);
     println!("{},{:?}",bitys_test.get(3),bitys_test);
 }
+
+use bitys::bitops::BitOps;
+#[test]
+fn bitops_get_set() {
+    let mut num:u8 = 8; //0001
+    println!("{}",num.get_bit(3));
+    assert_eq!(num.get_bit(0), false);
+    assert_eq!(num.get_bit(1), false);
+    assert_eq!(num.get_bit(2), false);
+    assert_eq!(num.get_bit(3), true);
+
+    num.set_bit(7);
+    println!("{}",num.get_bit(7));
+    assert_eq!(num.get_bit(7), true);
+    assert_eq!(num.get_bit(3), true);
+    println!("{}",num);
+    assert_eq!(num, (8+2_u8.pow(7))) 
+}
