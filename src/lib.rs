7dfddf76b12c85b3d3dd69581875b6dd71594dd3
Pogwat <208688662+Pogwat@users.noreply.github.com>
2026-06-25 01:41:46 -0400

    slicing bit start end

--- src/lib.rs
+++ src/lib.rs
@@ -6,9 +6,11 @@ popcnt
 Index \,
 IndexMut
 set/get \,
+iter(mut) \,
 */
 use bit_operations::BitOps;
 use bit_operations::MutBitProxy;
+use bit_operations::NumRangeExtract;
 mod slicing;
 use slicing::*;
 mod iter;
@@ -29,7 +31,7 @@ macro_rules! unints {($($type:ty),*) => {
         const BITDEX_MAX:usize = Self::ELEMENT_BITS.ilog2() as usize;
     })*} //[StartElem(64), StartBit(6), Len(58)]
 }
-unints!(u8,u16,u32,u64);
+unints!(u8,u16,u32,u64,u128,i8,i16,i32,i64,i128,usize);
 
 #[derive(Debug)]
 pub struct Bitys<ElementType: UInts> {
@@ -72,7 +74,7 @@ impl<ElementType: UInts> Bitys<ElementType> {
 
     pub fn iter(&self) -> Biter<ElementType> {
         Biter {
-            end_ptr:(&self.bytes[self.bytes.len()-1] as *const ElementType), 
+            end_ptr:&self.bytes[self.bytes.len()-1] as *const ElementType, 
             end_bit:Self::bit_idx(self.len)-1,
             ptr: &self.bytes[0] as *const ElementType,
             bit_position:0
@@ -88,6 +90,8 @@ impl<ElementType: UInts> Bitys<ElementType> {
             _marker: PhantomData
         }
     }
+
+
 } //Excluding genercis I have a full working bitvec in lines:25-39 just 14 lines of code!
 
 use std::ops::Index;
@@ -97,3 +101,23 @@ impl <'a, ElementType:UInts> Index<usize> for Bitys<ElementType> {
         if self.get(index) {&Self::TRUE} else {&Self::FALSE}
     }
 }
+
+macro_rules! bitys {
+    ($($type: ty),*) => {
+        $(
+            impl Bitys<$type> {
+                pub fn ctz<R:NumRangeExtract<usize>>(&self, range:R) -> usize {
+                    let start = range.start().unwrap_or(0).max(0);
+                    let end = range.end().unwrap_or(self.len).min(self.len);
+                    let start_type_idx =  Self::type_idx(start);
+                    let end_type_idx =  Self::type_idx(end);
+                    let end_exclude_bits = self.bytes[end_type_idx].ctz(&(Self::bit_idx(end)..));
+                    let start_exclude_bits = self.bytes[start_type_idx].ctz(&(0..=Self::bit_idx(start))); 
+                    let element_ctz:usize = self.bytes[start_type_idx..end_type_idx].iter().map(|block| (*block).count_zeros() as usize).sum::<usize>();
+                    element_ctz - start_exclude_bits - end_exclude_bits
+                }
+            }
+        )*
+    }
+}
+bitys!(u8,u16,u32,u64,u128,i8,i16,i32,i64,i128,usize);
--- src/slicing.rs
+++ src/slicing.rs
@@ -7,28 +7,17 @@ pub struct Mutable;
 pub struct Immutable;
 /* START, START BIT, END, END_BIT */
 pub struct BitSlice<Mutability> {
-    start_element:usize, //61 bits
-    start_bit:usize, //3 bits
-    end_element:usize, //61 bits
-    end_bit: usize, // 3 bits
+    end_bit:usize, //64bits
+    start_bit:usize, //64bits
     _mutability: PhantomData<Mutability>, // 0 bits
 }
 
 impl <Mutability> BitSlice<Mutability> {
-    fn wrap(&self) -> (usize,usize) {
-        let mut start = self.start_element;
-        start.set_these_bits(self.start_bit << 61, &(61..=63));
-        let mut end = self.end_element;
-        end.set_these_bits(self.end_bit << 61, &(61..=63));
-        (start,end)
-    }
-
-    pub fn to_pointer<'a, ElementType: UInts>(&self) -> &'a [ElementType] {unsafe { std::mem::transmute(self.wrap()) }}
-    pub fn to_mut_pointer<'a, ElementType: UInts>(&mut self) -> &'a mut [ElementType] {unsafe { std::mem::transmute(self.wrap()) }}
- 
+    pub fn to_pointer<'a, ElementType: UInts>(&self) -> &'a [ElementType] {unsafe { core::mem::transmute((self.start_bit,self.end_bit)) }}
+    pub fn to_mut_pointer<'a, ElementType: UInts>(&mut self) -> &'a mut [ElementType] {unsafe { std::mem::transmute((self.start_bit,self.end_bit)) }}
 }
 
-pub trait MutabilityFlag {type MutFlag;}
+ pub trait MutabilityFlag {type MutFlag;}
 
 pub trait BitSliceOps<Mutability>: MutabilityFlag {
     fn to_bitslice(&self) -> BitSlice<Mutability>;
@@ -42,10 +31,8 @@ macro_rules! slice_mutability {
             fn to_bitslice(&self) -> BitSlice<$mutability> {
                 let (start, end): (usize, usize) = unsafe {  *(self as *const Self as *const (usize, usize)) }; //Pointer to (self as 2 usize pointers) then deref
                 BitSlice {
-                    start_element: start.get_bits(&(0..61)), 
-                    start_bit: start.get_bits(&(61..=63)) >> 61, 
-                    end_element: end.get_bits(&(0..61)), 
-                    end_bit: end.get_bits(&(61..=63)) >> 61, 
+                    start_bit: start,
+                    end_bit: end,
                     _mutability: PhantomData, 
                 }
             }
